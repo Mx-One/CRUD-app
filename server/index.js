830ad95c2d3ab0671414b2aebc0197793465d00f
Max M <mx.mansbridge@gmail.com>
2024-07-24 20:25:30 -0400

    server v1.1

--- server/index.js
+++ server/index.js
@@ -44,7 +44,8 @@ app.get("/todos", async (req,res) => {
 app.get("/todos/:id", async (req,res) => {
   try {
     const {id} = req.params;
-    console.log(id)
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+    res.json(todo.rows[0])
   }
   catch(err) {
     console.error(err.message);
@@ -53,8 +54,28 @@ app.get("/todos/:id", async (req,res) => {
 })
 
 //update a todo
+app.put("/todos/:id", async (req,res) => {
+  try {
+    const {id} = req.params;
+    const {description} = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+    res.json("Todo was updated");
+  } 
+  catch(err) {
+    console.error(err.message);
+  }
+})
 
 //delete a todo
+app.delete("/todos/:id", async (req,res) => {
+  try {
+    const {id} = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    res.json("Todo was deleted");
+  } catch (err) {
+      console.error(err.message);
+  }
+})
 
 
 app.set('port', (process.env.PORT || 5000));
